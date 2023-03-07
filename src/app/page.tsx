7dad8de4377422d7a74975b3f9008b0e55536fb0
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-03-08 03:31:57 +0530

    made AI form & 3D scene :octocat:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,17 @@
 "use client";
-import {
-  ConnectKitButton,
-  ConnectKitProvider,
-  getDefaultClient,
-} from "connectkit";
+import SplineObject from "@/components/SplineObject";
 import { Inter } from "next/font/google";
-import Image from "next/image";
-import { WagmiConfig, configureChains, createClient } from "wagmi";
-import { fantom, fantomTestnet } from "wagmi/chains";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { publicProvider } from "wagmi/providers/public";
-import styles from "./page.module.css";
 
-import { usePrepareContractWrite, useAccount, useContractWrite } from "wagmi";
-import ABI from "../contracts/ABI.json";
 import { useState } from "react";
+import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
+import ABI from "../contracts/ABI.json";
 
 const contractAddress = "0xA4CCEb9e84b9682ca559AA41DB57f4BECe586dc5";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { address } = useAccount();
+  const { address, isConnected } = useAccount();
 
   const prepareContractWrite = usePrepareContractWrite({
     address: contractAddress,
@@ -40,24 +30,33 @@ export default function Home() {
   const [name, setName] = useState<string | null>('');
   const [desc, setDesc] = useState<string | null>('');
   return (
-    <main className='w-full h-full flex flex-col justify-between items-center p-10'>
-      <form className='w-full h-full flex flex-col gap-5 bg-white/10 md:w-1/2 p-10 rounded-md'>
-        <input
-          className='p-3 rounded-full mt-0 text-gray-800 shadow-sm placeholder:text-gray-500 placeholder:dark:text-gray-200 dark:text-white'
-          id="name"
-          name="name"
-          placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          className='p-3 rounded-full mt-0 text-gray-800 shadow-sm placeholder:text-gray-500 placeholder:dark:text-gray-200 dark:text-white'
-          id="desc"
-          name="desc"
-          placeholder="Description"
-          onChange={(e) => setDesc(e.target.value)}
-        />
-        <button onClick={handleSendTransaction}>Mint</button>
-      </form>
-    </main>
+    <div className='w-full h-full flex flex-col justify-between items-center py-10'>
+      <SplineObject scene='https://prod.spline.design/tfcra4szzFG8uSFH/scene.splinecode' />
+      {isConnected &&
+        <div className='w-full h-4/5 flex flex-row bg-black/20 md:w-2/3 gap-5 p-10 rounded-md z-1 fixed'>
+          <div className='w-full h-full flex flex-col gap-5 justify-evenly text-lg'>
+            <h1 className='text-5xl font-semibold text-white'>AI Minter</h1>
+            <input
+              className='px-5 py-3 rounded-lg mt-0 text-gray-800 shadow-sm placeholder:text-gray-500 placeholder:dark:text-gray-200 dark:text-white'
+              id="name"
+              name="name"
+              placeholder="Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <textarea
+              className='px-5 py-3 rounded-lg mt-0 text-gray-800 shadow-sm placeholder:text-gray-500 placeholder:dark:text-gray-200 dark:text-white'
+              id="desc"
+              name="desc"
+              rows={8}
+              placeholder="Description"
+              onChange={(e) => setDesc(e.target.value)}
+            />
+            <button className='bg-blue-700 hover:bg-blue-800 text-white py-2 rounded-lg' onClick={()=>{}}>Generate AI Art</button>
+            <button className='bg-blue-700 hover:bg-blue-800 text-white py-2 rounded-lg' onClick={handleSendTransaction}>Mint</button>
+          </div>
+          <img src='./out.png' alt='ai-art'/>
+        </div>
+      }
+    </div>
   );
 }
