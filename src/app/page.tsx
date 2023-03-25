99fac6fc201776c2b4019515e24332859b82e9fb
Saviour1001 <harshghodkar@gmail.com>
2023-03-25 17:53:56 +0530

    added 5ire chain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,46 @@
 import HamsterLoader from "@/components/HamsterLoader";
 import SplineObject from "@/components/SplineObject";
 import { Inter } from "next/font/google";
-import Confetti from 'react-confetti';
+import Confetti from "react-confetti";
 import { useState } from "react";
-import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
+import {
+  useAccount,
+  useContractWrite,
+  usePrepareContractWrite,
+  useNetwork,
+} from "wagmi";
 import ABI from "../contracts/ABI.json";
 import { ThirdwebStorage } from "@thirdweb-dev/storage";
 import { Configuration, OpenAIApi } from "openai";
 import Link from "next/link";
-const contractAddress = "0xA4CCEb9e84b9682ca559AA41DB57f4BECe586dc5";
+const contractAddresses = {
+  997: "0x1a8784a45731F889D4a92258AE7E149d5C737AA1", // 5ire
+  5001: "0x9B37654fc8d92cfdd3CF7bc8b507aefc34E795E0", // Mantle
+  5: "0xA72e987B2c8e289C16FFb9107eF9942dB8872128", // Goerli
+};
+
+const explorerURLs = {
+  997: "https://explorer.5ire.network/evm/tx/", // 5ire
+  5001: "https://explorer.testnet.mantle.xyz/tx/", // Mantle
+  5: "https://goerli.etherscan.io/tx/", // Goerli
+};
+
 // const contractAddress = "0x1a8784a45731F889D4a92258AE7E149d5C737AA1";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { chain } = useNetwork();
+
   const { address, isConnected } = useAccount();
   const [name, setName] = useState<string | null>("Your AI Mint");
   const [desc, setDesc] = useState<string | null>("Your AI Mint");
   const [metadata, setMetadata] = useState<string | null>("");
-  const [image, setImage] = useState<string|null>(null);
+  const [image, setImage] = useState<string | null>(null);
   const [loading, setLoading] = useState<number>(0);
   const [explosion, setExplosion] = useState<boolean>(false);
   const [disabled, setDisabled] = useState<boolean>(true);
-  const [forged, setForged] = useState<string|null>(null);
+  const [forged, setForged] = useState<string | null>(null);
   const configuration = new Configuration({
     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   });
@@ -62,13 +80,13 @@ export default function Home() {
   };
 
   const prepareContractWrite = usePrepareContractWrite({
-    address: contractAddress,
+    address: "0x1a8784a45731F889D4a92258AE7E149d5C737AA1",
     abi: ABI,
     functionName: "safeMint",
     args: [address, metadata],
   });
   const { data, isLoading, isSuccess, writeAsync } = useContractWrite(
-      prepareContractWrite.config
+    prepareContractWrite.config
   );
 
   const handleSendTransaction = async () => {
@@ -130,38 +148,40 @@ export default function Home() {
               Generate {!disabled && "new"} AI Art
             </button>
             {/* <button onClick={awesome}>Tester</button> */}
-            {forged?
-                <Link href={forged!}>
+            {forged ? (
+              <Link href={forged!}>
                 <div className="bg-blue-700 hover:bg-blue-800 text-white py-2 rounded-lg disabled:bg-gray-500 flex justify-center">
                   View on Block Explorer
                 </div>
-                </Link>
-                :
+              </Link>
+            ) : (
               <button
                 className="bg-blue-700 hover:bg-blue-800 text-white py-2 rounded-lg disabled:bg-gray-500"
                 onClick={handleSendTransaction}
                 disabled={disabled}
-            >
-              Forge
-            </button>}
+              >
+                Forge
+              </button>
+            )}
           </div>
-          <img src={image||"./out.png"} onLoad={()=>setLoading(0)} alt="ai-art" />
+          <img
+            src={image || "./out.png"}
+            onLoad={() => setLoading(0)}
+            alt="ai-art"
+          />
         </div>
-      )
-      }
-      {loading===1 && (
+      )}
+      {loading === 1 && (
         <div className="w-1/3 h-1/3 flex justify-center items-center absolute top-1/3 left-1/3 z-10">
           <HamsterLoader loaderTitle="Forging the Image" />
         </div>
       )}
-      {loading===2 && (
+      {loading === 2 && (
         <div className="w-1/3 h-1/3 flex justify-center items-center absolute top-1/3 left-1/3 z-10">
           <HamsterLoader loaderTitle="Uploading to IPFS" />
         </div>
       )}
-      {explosion &&
-            <Confetti className="fullscreen"/>
-      }
+      {explosion && <Confetti className="fullscreen" />}
     </div>
   );
 }
